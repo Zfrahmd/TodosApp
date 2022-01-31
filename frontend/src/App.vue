afd7ef526f0ab7261e0ea91de9d8cfa2d2d37846
Zafir <zfr.ahmd5661@gmail.com>
2022-01-31 23:16:45 +0500

    Finalized TodoList App

---
+++ frontend/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Todo List Manager</h1>
+      <h5> Powered by Rails 6.1.4 | Vue.js 3.0 | Vuex 4.0 | Axios | SQLite3</h5>
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from '@/components/Todos.vue';
+import AddTodo from '@/components/AddTodo.vue';
+import FilterTodos from '@/components/FilterTodos.vue';
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #e8f7f0;
+  line-height: 1.6;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+
+</style>
---
+++ frontend/src/components/AddTodo.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="">
+        <h3>Add Todo</h3>
+        <div class="add">
+            <form @submit="onSubmit">
+                <input type="text" v-model="title" placeholder="Add Todo...">
+                <input type="submit" value="submit">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: 'AddTodo',
+    data() {
+        return {
+            title: ""
+        }
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        onSubmit(event) {
+            event.preventDefault();
+            this.addTodo(this.title);
+            this.title = "";
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+form {
+    display: flex;
+    padding: 5px;
+    border: 1px solid #ccc;
+    outline: 0;
+}
+
+input[type="text"] {
+    border: 1px solid #444;
+    width: 100%;
+    padding: 10px;    
+}
+
+input[type="text"]:hover, input[type="text"]:focus {
+    border: 1px solid #41b883;
+    
+}
+
+input[type="submit"] {
+    background: #41b883;
+    color: #fff;
+    border: 1px solid #41b883;
+    cursor: pointer;
+    padding: 10px;
+    margin-left: 1%;
+}
+
+input[type="submit"]:hover, input[type="submit"]:focus {
+    background: #318b63;    
+}
+
+</style>
